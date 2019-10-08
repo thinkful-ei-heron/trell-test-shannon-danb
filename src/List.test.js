25cd1007893bf96ffa0ac5ac482752cdc984c759
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-10-08 09:33:07 -0600

    smoke testing done

---
+++ src/List.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './List';
+
+
+it('renders without crashing', () => {
+const div = document.createElement('div');
+const cards = [{id:'a', title:'title', content:'content'}];
+ReactDOM.render(<List header= 'this is the header' cards= {cards} key='a' />, div);
+ReactDOM.unmountComponentAtNode(div);
+});
